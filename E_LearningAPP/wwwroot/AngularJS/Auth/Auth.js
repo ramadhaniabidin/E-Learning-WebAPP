09c8e1284325cc124df66b38e3aecd91d3e517f0
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-07-19 10:43:10 +0700

    consume data from API

--- E_LearningAPP/wwwroot/AngularJS/Auth/Auth.js
+++ E_LearningAPP/wwwroot/AngularJS/Auth/Auth.js
@@ -1,22 +1,38 @@
 ﻿var app = angular.module('app', ['angular.filter']);
 
 app.service('svc', function ($http) {
+    this.svc_GetAllAccounts = function () {
+        var response = $http({
+            method: 'GET',
+            url: 'https://192.168.142.232:7290/E-LearningAPI/Account/GetAllAccounts',
+            data: {},
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'
+        });
 
+        return response;
+    }
 })
 
 app.controller('ctrl', function ($scope, svc) {
+
+    $scope.GetAccounts = function () {
+        var promise = svc.svc_GetAllAccounts();
+        promise.then(function (response) {
+            console.log(response.data);
+        });
+    };
+
+
+
     const loginBtn = angular.element(document.getElementById('login'));
     const signUpBtn = angular.element(document.getElementById('signup'));
-
-    console.log(loginBtn[0].parentNode.parentNode);
-    console.log(signUpBtn[0].parentNode);
 
     $scope.handleLogin = function () {
         let parent = loginBtn[0].parentNode.parentNode;
         let notParent = signUpBtn[0].parentNode;
         console.log(parent);
         console.log(notParent);
-        /*console.log("Login Button after click: " + parent);*/
         const element = parent.classList;
 
         if (!parent.classList.contains('slide-up')) {
@@ -29,24 +45,6 @@ app.controller('ctrl', function ($scope, svc) {
             document.getElementById('login').style.pointerEvents = "none";
             document.getElementById('signup').style.pointerEvents = "";
         }
-        
-        //console.log(element.value);
-
-        //if (element.contains('slide-up')) {
-        //    console.log("Success");
-        //} else {
-        //    console.log('Error');
-        //}
-
-
-        //angular.element(parent.classList).find((element) => {
-        //    if (element !== "slide-up") {
-        //        parent.classList.add('slide-up');
-        //    } else {
-        //        signUpBtn[0].parentNode.classList.add('slide-up');
-        //        parent.classList.remove('slide-up');
-        //    }
-        //});
     };
 
     $scope.handleSignUp = function () {
@@ -54,7 +52,6 @@ app.controller('ctrl', function ($scope, svc) {
         let notParent = loginBtn[0].parentNode;
         console.log(parent);
         console.log(notParent);
-        /*console.log("Sign Up button after click: " + parent);*/
         if (!parent.classList.contains('slide-up')) {
             parent.classList.add('slide-up');
         } else {
@@ -64,13 +61,8 @@ app.controller('ctrl', function ($scope, svc) {
             document.getElementById('signup').style.pointerEvents = "none";
             parent.classList.remove('slide-up');
         }
-        //angular.element(parent.classList).find((element) => {
-        //    if (element !== "slide-up") {
-        //        parent.classList.add('slide-up');
-        //    } else {
-        //        loginBtn[0].parentNode.classList.add('slide-up');
-        //        parent.classList.remove('slide-up');
-        //    }
-        //});
     }
+
+
+    $scope.GetAccounts();
 })
