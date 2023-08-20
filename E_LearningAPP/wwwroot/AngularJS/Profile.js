c2721c4bc216116c5374f7896e96e6814a9ba7de
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-20 17:54:51 +0700

    success update/insert user profile to database

--- E_LearningAPP/wwwroot/AngularJS/Profile.js
+++ E_LearningAPP/wwwroot/AngularJS/Profile.js
@@ -1,10 +1,29 @@
 ﻿/*var app = angular.module('app', ['angular.filter']);*/
 
 app.service('svc', function ($http) {
+    this.svc_InsertOrUpdateProfile = function (accountDetail, token) {
+        var param = {
+            'accountDetail': accountDetail,
+            'token': token
+        };
+
+        var response = $http({
+            method: 'POST',
+            url: 'https://192.168.1.2:7290/E-LearningAPI/Account/InsertOrUpdateProfile/token/' + encodeURIComponent(token),
+            data: param.accountDetail,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'
+        });
+
+        console.log('Param: ', param);
+
+        return response;
+    }
+
     this.svc_GetAllProvinsi = function () {
         var response = $http({
             method: 'GET',
-            url: 'https://192.168.1.16:7290/E-LearningAPI/Address/GetAllProvince',
+            url: 'https://192.168.1.2:7290/E-LearningAPI/Address/GetAllProvince',
             data: {},
             contentType: 'application/json; charset=utf-8',
             dataType: 'json'
@@ -16,7 +35,7 @@ app.service('svc', function ($http) {
     this.svc_GetKabupatenByProvinsiName = function (provinsiName) {
         var response = $http({
             method: 'GET',
-            url: 'https://192.168.1.16:7290/E-LearningAPI/Address/ListKabupatenByProvinsiName/provinsiName/' + encodeURIComponent(provinsiName),
+            url: 'https://192.168.1.2:7290/E-LearningAPI/Address/ListKabupatenByProvinsiName/provinsiName/' + encodeURIComponent(provinsiName),
             data: {},
             contentType: 'application/json; charset=utf-8',
             dataType: 'json'
@@ -28,7 +47,7 @@ app.service('svc', function ($http) {
     this.svc_GetKecamatanByKabupatenName = function (provinsiName, kabupatenName) {
         var response = $http({
             method: 'GET',
-            url: 'https://192.168.1.16:7290/E-LearningAPI/Address/GetAllKecamatan/provinsi/' + encodeURIComponent(provinsiName) + '/kabupaten/' + encodeURIComponent(kabupatenName),
+            url: 'https://192.168.1.2:7290/E-LearningAPI/Address/GetAllKecamatan/provinsi/' + encodeURIComponent(provinsiName) + '/kabupaten/' + encodeURIComponent(kabupatenName),
             data: {},
             contentType: 'application/json; charset=utf-8',
             dataType: 'json'
@@ -40,7 +59,7 @@ app.service('svc', function ($http) {
     this.svc_GetDesa = function (provinsiName, kabupatenName, kecamatanName) {
         var response = $http({
             method: 'GET',
-            url: 'https://192.168.1.16:7290/E-LearningAPI/Address/GetAllDesa/provinsi/' + encodeURIComponent(provinsiName) + '/kabupaten/' + encodeURIComponent(kabupatenName) + '/kecamatan/' + encodeURIComponent(kecamatanName),
+            url: 'https://192.168.1.2:7290/E-LearningAPI/Address/GetAllDesa/provinsi/' + encodeURIComponent(provinsiName) + '/kabupaten/' + encodeURIComponent(kabupatenName) + '/kecamatan/' + encodeURIComponent(kecamatanName),
             data: {},
             contentType: 'application/json; charset=utf-8',
             dataType: 'json'
@@ -130,6 +149,36 @@ app.controller('ctrl', function ($scope, svc, sharedService) {
                 console.log('List Desa: ', $scope.listDesa);
             });
         }
+    }
+
+    $scope.SaveProfile = function () {
+        var param = {
+            'userData': {
+                'nama': $scope.namaPengguna,
+                'no_telp': $scope.noTelpPengguna,
+                'provinsi': $scope.selectedProvinsi,
+                'kabupaten': $scope.selectedKabupaten,
+                'kecamatan': $scope.selectedKecamatan,
+                'desa': $scope.selectedDesa,
+                'email': $scope.emailPengguna
+            },
+
+            'token': sessionStorage.getItem('LoginToken')
+
+        }
+
+        //console.log('Param: ', param);
+        var promise = svc.svc_InsertOrUpdateProfile(param.userData, param.token);
+        promise.then(function (response) {
+            var resp_data = response.data;
+            if (resp_data.Success) {
+                alert(resp_data.Message);
+            }
+
+            else {
+                alert(response.Message);
+            }
+        });
     }
 
     $scope.GetAllProvinsiOnLoad();
