0b7aed2e8736ffbc0ecad14765061d0c7788ce91
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-02 23:09:16 +0700

    Edit home page

--- E_LearningAPP/wwwroot/AngularJS/Home.js
+++ E_LearningAPP/wwwroot/AngularJS/Home.js
@@ -2,10 +2,42 @@
 
 
 app.service('svc', function ($http) {
+    this.svc_OnLoadGetProfile = function (token) {
+        var response = $http({
+            method: 'GET',
+            url: 'https://192.168.1.3:7290/E-LearningAPI/Account/GetAccountDetailByToken/token/' + encodeURIComponent(token),
+            data: {},
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'
+        });
 
+        return response;
+    }
 });
 
 app.controller('HomeController', function ($scope, svc) {
+    // Variable declaration
+    $scope.userName = "";
+    // End region
+
+    // This function retrieves the user data
+    $scope.GetProfile_OnLoad = function () {
+        var token = sessionStorage.getItem('LoginToken');
+        var promise = svc.svc_OnLoadGetProfile(token);
+        promise.then(function (response) {
+            var resp_data = response.data;
+            if (resp_data.Success) {
+                console.log('Response Data: ', resp_data);
+                var accountDetail = resp_data.accountDetail;
+
+                $scope.userName = accountDetail.nama;
+            }
+        });
+    }
+    // End region
+
+
+
     $scope.GetToken_OnLoad = function () {
         var token = sessionStorage.getItem('LoginToken');
         if ((token !== null) && (token !== undefined) && (token !== '')) {
@@ -19,4 +51,5 @@ app.controller('HomeController', function ($scope, svc) {
     }
 
     /*$scope.GetToken_OnLoad();*/
+    $scope.GetProfile_OnLoad();
 });
