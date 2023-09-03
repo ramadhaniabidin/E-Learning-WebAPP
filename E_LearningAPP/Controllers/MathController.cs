ab71b643bc73ea16277f51a0cbc972ff5a837e85
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-03 15:18:54 +0700

    add MathController to manage routing for Math subject

---
+++ E_LearningAPP/Controllers/MathController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_LearningAPP.Controllers
+{
+    public class MathController : Controller
+    {
+        [Route("Course/Physics/{grade}")]
+        public IActionResult Index(int grade)
+        {
+            return View($"/Views/Math/{grade}/Index.cshtml");
+        }
+    }
+}
