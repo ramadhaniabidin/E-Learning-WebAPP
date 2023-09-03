e799764decb16d99c2d023151f1fded1e4260c83
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-03 15:28:23 +0700

    add page branching for Chemistry subject

---
+++ E_LearningAPP/Controllers/ChemistryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_LearningAPP.Controllers
+{
+    public class ChemistryController : Controller
+    {
+        [Route("Course/Chemistry/{grade}")]
+        public IActionResult Index(int grade)
+        {
+            return View($"/Views/Chemistry/{grade}/Index.cshtml");
+        }
+    }
+}
