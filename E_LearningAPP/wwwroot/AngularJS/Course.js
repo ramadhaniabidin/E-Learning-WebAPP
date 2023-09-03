7567f33838f2f97ea4dc02253a65a6073299a166
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-03 14:11:07 +0700

    make dynamic function to toggle the accordion

--- E_LearningAPP/wwwroot/AngularJS/Course.js
+++ E_LearningAPP/wwwroot/AngularJS/Course.js
@@ -5,8 +5,28 @@ app.service('svc', function ($http) {
 });
 
 app.controller('CourseController', function ($scope, svc) {
+    $scope.ToggleAccordion = function (subject) {
+        var panel;
+        if (subject == 'physics') {
+            panel = document.getElementById('physics-panel');
+        }
+        else if (subject == 'math') {
+            panel = document.getElementById('math-panel');
+        }
+        else if (subject == 'chemistry') {
+            panel = document.getElementById('chemistry-panel');
+        }
+
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        }
+        else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    }
+
+
     $scope.ShowPhysicsAccordion = function () {
-        var accordion = document.getElementById('btn-accordion');
         var panel = document.getElementById('panel1');
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
