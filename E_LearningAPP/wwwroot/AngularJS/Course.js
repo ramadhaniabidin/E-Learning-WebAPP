b3729d59094eaed1bcf1469442651220077bc949
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-03 13:53:23 +0700

    add javascript for Course page

---
+++ E_LearningAPP/wwwroot/AngularJS/Course.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('app', ['ngRoute', 'angular.filter']);
+
+app.service('svc', function ($http) {
+
+});
+
+app.controller('CourseController', function ($scope, svc) {
+    $scope.ShowPhysicsAccordion = function () {
+        var accordion = document.getElementById('btn-accordion');
+        var panel = document.getElementById('panel1');
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        }
+
+        else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    };
+});
