7fd3f3e36bfc2eaa4f69d8ae79aaea2d34056bf4
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-05 21:08:49 +0700

    add options to choose curricullum

--- E_LearningAPP/wwwroot/AngularJS/Course.js
+++ E_LearningAPP/wwwroot/AngularJS/Course.js
@@ -25,6 +25,38 @@ app.controller('CourseController', function ($scope, svc) {
         }
     }
 
+    $scope.ToggleCurricullum = function (grade) {
+        var parentPanel = document.getElementById('physics-panel');
+        var panel;
+        if (grade == 10) {
+            panel = document.getElementById('physics-10-panel');
+        }
+        else if (grade == 11) {
+            panel = document.getElementById('physics-11-panel');
+        }
+        else if (grade == 12) {
+            panel = document.getElementById('physics-12-panel');
+        }
+
+
+        
+        console.log(panel);
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            panel.style.marginBottom = null;
+            console.log('Panel Scrollheight', panel.scrollHeight);
+        }
+        else {
+            console.log('Panel Scrollheight', panel.scrollHeight);
+            var parentMaxHeight = parentPanel.scrollHeight;
+            var childMaxHeight = panel.scrollHeight;
+            var totalMaxHeight = parentMaxHeight + childMaxHeight;
+
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+            panel.style.marginBottom = '2.5%';
+            parentPanel.style.maxHeight = totalMaxHeight + 'px';
+        }
+    };
 
     $scope.ShowPhysicsAccordion = function () {
         var panel = document.getElementById('panel1');
