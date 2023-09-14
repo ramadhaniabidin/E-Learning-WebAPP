f8f2e9801347b082b40cc7ce9977ec8e71469d70
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-14 22:44:42 +0700

    complete routing page for Math subject

--- E_LearningAPP/Controllers/MathController.cs
+++ E_LearningAPP/Controllers/MathController.cs
@@ -4,7 +4,7 @@ namespace E_LearningAPP.Controllers
 {
     public class MathController : Controller
     {
-        [Route("Course/Math/curr/{grade}")]
+        [Route("Course/Math/{curr}/{grade}")]
         public IActionResult Index(string curr, int grade)
         {
             return View($"/Views/Math/{curr}/{grade}/Index.cshtml");
