68b2c8bff23f1d843bb80b2500621ddfa73bd624
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-23 09:00:46 +0700

    add MasterPage view

---
+++ E_LearningAPP/Views/Shared/MasterPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_LearningAPP.Views.Shared
+{
+    public class MasterPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
