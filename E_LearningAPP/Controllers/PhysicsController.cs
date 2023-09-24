2a030804b988a73e2f343723786a13c4b0935616
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-09-24 12:16:03 +0700

    add method in PhysicsController

--- E_LearningAPP/Controllers/PhysicsController.cs
+++ E_LearningAPP/Controllers/PhysicsController.cs
@@ -11,5 +11,12 @@ namespace E_LearningAPP.Controllers
             return View($"/Views/Physics/{curr}/{grade}/Index.cshtml");
             //return View();
         }
+
+        [Route("Course/Physics/{curr}/{grade}/Topics/{topicName}")]
+        public IActionResult Topic(string topicName, string curr, int grade)
+        {
+            Console.WriteLine($"Curricullum = {curr}, Grade = {grade}, Topic = {topicName}");
+            return View($"/Views/Physics/{curr}/{grade}/Topics/{topicName}");
+        }
     }
 }
