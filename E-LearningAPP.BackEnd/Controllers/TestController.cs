0c483d572e52821dd9ce245d16bde69a38ac4c01
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2023-12-16 22:18:23 +0700

    Detach API Project
    Create new BackEnd Project

---
+++ E-LearningAPP.BackEnd/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using E_LearningAPP.BackEnd.Logics.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_LearningAPP.BackEnd.Controllers
+{
+    [Route("E-learning/BackEnd/[controller]/[action]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        private readonly ITestRepository _itestRepo;
+        public TestController(ITestRepository itestRepo)
+        {
+            _itestRepo = itestRepo;
+        }
+        [HttpGet]
+        public IActionResult TestMethod()
+        {
+            var response = _itestRepo.TestMethod();
+            return Ok(response);
+        }
+    }
+}
